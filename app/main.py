f09098223e7f4f00a3355270ab7d29e4b73b1e02
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2026-06-27 12:03:38 +0530

    feat: prepare for deployment

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ app = FastAPI(
 # Add CORS middleware to allow the frontend to access the API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"], 
-    allow_credentials=True,
+    allow_origins=["*"], 
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
